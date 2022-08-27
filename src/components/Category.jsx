73257d8d9e3903a150324c349ece4447ec3fd472
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2022-08-27 10:08:19 +0530

    Functional application is ready but need some styling

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -41,10 +41,28 @@ const SLink = styled(NavLink)`
   margin-right: 2rem;
   text-decoration: none;
   background: linear-gradient(35deg, #494949, #313131);
-  widht: 5rem;
-  height: 5rem;
+  width: 6rem;
+  height: 6rem;
   cursor: pointer;
   transform: scale(0.8);
+
+  h4 {
+    color: white;
+    font-size: 0.8rem;
+  }
+  svg {
+    color: white;
+    font-size: 1.8rem;
+  }
+  &.active {
+    background: linear-gradient(to right, orange, red);
+    svg {
+      color: white;
+    }
+    h4 {
+      color: white;
+    }
+  }
 `;
 
 export default Category;
